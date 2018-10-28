d8c746b83f5164235101645eba646431edb4fd39
ichyo <ichyo00@gmail.com>
2018-10-28 22:40:08 +1100

    Update get_standings

--- src/contests.rs
+++ src/contests.rs
@@ -1,8 +1,56 @@
-fn get_standings_url(id: &str) -> String {
-    format!("https://beta.atcoder.jp/contests/{}/standings/json", id)
+use std::collections::HashMap;
+
+#[derive(Deserialize, Debug, Eq, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct Standings {
+    fixed: bool,
+    task_info: Vec<TaskInfo>,
+    standings_data: Vec<StandingsData>,
 }
 
-pub fn get_standings(id: &str) -> String {
+#[derive(Deserialize, Debug, Eq, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct TaskInfo {
+    assignment: String,
+    task_name: String,
+    task_screen_name: String,
+}
+
+#[derive(Deserialize, Debug, Eq, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct StandingsData {
+    rank: u64,
+    user_name: String,
+    user_screen_name: String,
+    task_results: HashMap<String, TaskResult>,
+}
+
+#[derive(Deserialize, Debug, Eq, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct TaskResult {
+    score: i64,
+}
+
+fn get_standings_url(id: &str) -> String {
+    format!(
+        "https://beta.atcoder.jp/contests/{id}/standings/json",
+        id = id
+    )
+}
+
+pub fn get_standings(id: &str) -> Result<Standings, reqwest::Error> {
     let url = get_standings_url(id);
-    url
+    let mut response = reqwest::get(&url)?;
+    let standings = response.json()?;
+    Ok(standings)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_standings() {
+        let _ = get_standings("tenka1-2018").unwrap();
+    }
 }
